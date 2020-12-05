2fe121bcc124d36c0d849fe05c99d40865b5ce45
dongshufeng <932230728@qq.com>
2020-12-05 15:47:10 +0800

    First commit : Install npm package && Add hello.js

---
+++ hello.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-05 15:43:09
+ * @LastEditTime: 2020-12-05 15:46:13
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \myLearn-GraphQL\hello.js
+ */
+
+ const { graphql, buildSchema } = require('graphql')
+
+ const scheam = buildSchema(`
+    type Query {
+        hello: String
+    }
+ `)
+
+ const root = { hello : ()=> `Hello world!`}
+
+ graphql(scheam,`{ hello }`,root).then((response) => {
+     console.log(response)
+ })
